32958c1b3a2b91c33dbd45aaa90e152e78bf7f01
Namida Aneskans <naan@bluegarden.com>
2018-10-07 13:37:05 +0200

    Add functionality for handling lists and dicts, as well as saving only changed values to the user config file.

--- confjson/__init__.py
+++ confjson/__init__.py
@@ -1,39 +1,65 @@
-import collections
 import json
 import pathlib
 
 
+DEFAULT_CONFIG_FILENAME = "default.config.json"
+USER_CONFIG_FILENAME = "user.config.json"
+
+
 class Config:
     def __init__(self, path):
-        self.dir = pathlib.Path(path)
-        if not self.dir.is_dir():
-            self.dir = self.dir.parent
-        self.default_config_path = self.dir / "default.config.json"
-        self.user_config_path = self.dir / "user.config.json"
+        pathlib_path = pathlib.Path(path)
+
+        if pathlib_path.is_dir():
+            self.directory = pathlib_path
+        elif pathlib_path.is_file():
+            self.directory = pathlib_path.parent
+        elif not pathlib_path.exists():
+            raise ValueError(
+                "Parameter `path` must be the path of an existing file"
+                f" or directory; '{path}' does not exist.")
+        else:
+            raise ValueError(
+                "Parameter `path` must be the path of an existing file"
+                f" or directory; '{path}' exists but is not a file or"
+                " a directory.")
+
+        self.default_config_path = self.directory / DEFAULT_CONFIG_FILENAME
+        self.user_config_path = self.directory / USER_CONFIG_FILENAME
+
+        self._default_dict = {}
+        self._user_dict = {}
 
         self.load()
 
     def __getitem__(self, key):
-        return self._chainmap[key]
+        if key not in self._user_dict:
+            default_value = self._default_dict[key]
+            self._user_dict[key] = json.loads(json.dumps(default_value))
+        return self._user_dict[key]
 
     def __setitem__(self, key, value):
-        self._chainmap[key] = value
+        self._user_dict[key] = value
 
     def load(self):
-        if self.default_config_path.exists():
+        try:
             with self.default_config_path.open() as file:
-                default_config = json.load(file)
-        else:
-            default_config = {}
+                self._default_dict = json.load(file)
+        except FileNotFoundError:
+            self._default_dict = {}
 
-        if self.user_config_path.exists():
+        try:
             with self.user_config_path.open() as file:
-                user_config = json.load(file)
-        else:
-            user_config = {}
-
-        self._chainmap = collections.ChainMap(user_config, default_config)
+                self._user_dict = json.load(file)
+        except FileNotFoundError:
+            self._user_dict = {}
 
     def save(self):
-        with self.user_config_path.open(mode="w") as file:
-            json.dump(self._chainmap.maps[0], file)
+        diff = {
+            key: value for key, value in self._user_dict.items()
+            if key not in self._default_dict
+            or value != self._default_dict[key]
+        }
+        if diff or self.user_config_path.exists():
+            with self.user_config_path.open(mode="w") as file:
+                json.dump(diff, file)
--- tests/test_confjson.py
+++ tests/test_confjson.py
@@ -1,6 +1,8 @@
 import json
 import os.path
 
+import pytest
+
 import confjson
 
 DEFAULT_CONFIG_FILENAME = "default.config.json"
@@ -8,30 +10,37 @@ DEFAULT_CONFIG_FILENAME = "default.config.json"
 USER_CONFIG_FILENAME = "user.config.json"
 
 DEFAULT_CONFIG = {
-    "key_unique_to_default": "value_unique_to_default",
-    "key_in_both_configs": "default_value",
-    "list": [
-        "default list[0]",
-        "default list[1]"
-    ],
-    "dict": {
-        "key1": "default dict[key1]",
-        "key2": "default dict[key2]"
+    "string_in_both": "d[string_in_both]",
+    "string_in_default": "d[string_in_default]",
+    "list_in_both": ["d[list_in_both][0]", "d[list_in_both][1]"],
+    "list_in_default": ["d[list_in_default][0]", "d[list_in_default][1]"],
+    "dict_in_both": {
+        "key_in_both": "d[dict_in_both][key_in_both]",
+        "key_in_default": "d[dict_in_both][key_in_default]"
+    },
+    "dict_in_default": {
+        "key_d1": "d[dict_in_default][key_d1]",
+        "key_d2": "d[dict_in_default][key_d2]"
     }
 }
 
 USER_CONFIG = {
-    "key_unique_to_user": "value_unique_to_user",
-    "key_in_both_configs": "user_value",
-    "list": [
-        "user list[0]",
-        "user list[1]"
-    ],
-    "dict": {
-        "key1": "user dict[key1]",
-        "key2": "user dict[key2]"
+    "string_in_both": "u[string_in_both]",
+    "string_in_user": "u[string_in_user]",
+    "list_in_both": ["u[list_in_both][0]", "u[list_in_both][0]"],
+    "list_in_user": ["u[list_in_user][0]", "u[list_in_user][1]"],
+    "dict_in_both": {
+        "key_in_both": "u[dict_in_both][key_in_both]",
+        "key_in_user": "u[dict_in_both][key_in_user]"
+    },
+    "dict_in_user": {
+        "key_u1": "u[dict_in_user][key_u1]",
+        "key_u2": "u[dict_in_user][key_u2]"
     }
 }
+
+XFAIL_CLEANUP_NOT_IMPLEMENTED = pytest.mark.xfail(
+    reason="Cleanup of user config file not yet implemented.", strict=True)
 
 
 def _generate_default_config(folder):
@@ -44,20 +53,30 @@ def _generate_user_config(folder):
         json.dump(USER_CONFIG, file)
 
 
-def test_containing_directory_is_used_if_path_argument_is_a_file(tmpdir):
+def _generate_both_config_files(folder):
+    _generate_default_config(folder)
+    _generate_user_config(folder)
+
+
+def test_init_with_file_path(tmpdir):
     filename = os.path.join(tmpdir, "file.py")
     with open(filename, "w") as file:
         file.write("\\[T]/")
     conf = confjson.Config(filename)
-    assert str(conf.dir) == tmpdir
+    assert str(conf.directory) == tmpdir
 
 
-def test_path_is_used_as_is_if_it_is_a_directory(tmpdir):
+def test_init_with_directory_path(tmpdir):
     conf = confjson.Config(tmpdir)
-    assert str(conf.dir) == tmpdir
+    assert str(conf.directory) == tmpdir
 
 
-def test_user_config_is_created_if_missing(tmpdir):
+def test_init_with_nonexistant_file_path(tmpdir):
+    with pytest.raises(ValueError):
+        conf = confjson.Config(os.path.join(tmpdir, "does_not_exist.wtf"))
+
+
+def test_create_user_config_file_if_missing(tmpdir):
     conf = confjson.Config(tmpdir)
     conf["a"] = "b"
     conf.save()
@@ -66,28 +85,26 @@ def test_user_config_is_created_if_missing(tmpdir):
     assert user_json["a"] == conf["a"]
 
 
-def test_user_config_is_loaded_if_extant(tmpdir):
+def test_load_user_config_file_if_it_exists(tmpdir):
     _generate_user_config(tmpdir)
     conf = confjson.Config(tmpdir)
-    assert conf["key_unique_to_user"] == "value_unique_to_user"
+    assert conf["string_in_user"] == USER_CONFIG["string_in_user"]
 
 
-def test_default_config_is_loaded_if_extant(tmpdir):
+def test_load_default_config_if_it_exists(tmpdir):
     _generate_default_config(tmpdir)
     conf = confjson.Config(tmpdir)
-    assert conf["key_unique_to_default"] == "value_unique_to_default"
+    assert conf["string_in_default"] == DEFAULT_CONFIG["string_in_default"]
 
 
-def test_user_config_takes_priority_on_overlap(tmpdir):
-    _generate_default_config(tmpdir)
-    _generate_user_config(tmpdir)
+def test_use_value_from_user_config_if_key_is_in_both_configs(tmpdir):
+    _generate_both_config_files(tmpdir)
     conf = confjson.Config(tmpdir)
-    assert conf["key_in_both_configs"] == "user_value"
+    assert conf["string_in_both"] == USER_CONFIG["string_in_both"]
 
 
-def test_changes_are_saved_only_to_user_config(tmpdir):
-    _generate_default_config(tmpdir)
-    _generate_user_config(tmpdir)
+def test_save_changes_only_to_user_config(tmpdir):
+    _generate_both_config_files(tmpdir)
     conf = confjson.Config(tmpdir)
     conf["new_key"] = "new_value"
     conf.save()
@@ -99,3 +116,135 @@ def test_changes_are_saved_only_to_user_config(tmpdir):
     with open(os.path.join(tmpdir, DEFAULT_CONFIG_FILENAME)) as file:
         default_json = json.load(file)
     assert "new_key" not in default_json
+
+
+def test_get_item_from_list_present_in_both_configs(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert conf["list_in_both"][0] == USER_CONFIG["list_in_both"][0]
+
+
+def test_get_item_from_list_present_only_in_default_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert conf["list_in_default"][0] == DEFAULT_CONFIG["list_in_default"][0]
+
+
+def test_get_item_from_list_present_only_in_user_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert conf["list_in_user"][0] == USER_CONFIG["list_in_user"][0]
+
+
+def test_add_item_to_list_present_in_both_configs(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["list_in_both"].append("new_item")
+    assert (
+        conf._user_dict["list_in_both"]
+        == USER_CONFIG["list_in_both"] + ["new_item"])
+    assert (
+        conf._default_dict["list_in_both"] == DEFAULT_CONFIG["list_in_both"])
+
+
+def test_add_item_to_list_present_only_in_user_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["list_in_user"].append("new_item")
+    assert (
+        conf._user_dict["list_in_user"]
+        == USER_CONFIG["list_in_user"] + ["new_item"])
+    assert "list_in_user" not in conf._default_dict
+
+
+def test_add_item_to_list_present_only_in_default_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["list_in_default"].append("new_item")
+    assert (
+        conf._user_dict["list_in_default"]
+        == DEFAULT_CONFIG["list_in_default"] + ["new_item"])
+    assert (
+        conf._default_dict["list_in_default"]
+        == DEFAULT_CONFIG["list_in_default"])
+
+
+def test_get_item_from_dict_present_in_both_configs(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert (
+        conf["dict_in_both"]["key_in_both"]
+        == USER_CONFIG["dict_in_both"]["key_in_both"])
+
+
+def test_get_item_from_dict_present_only_in_default_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert (
+        conf["dict_in_default"]["key_d1"]
+        == DEFAULT_CONFIG["dict_in_default"]["key_d1"])
+
+
+def test_get_item_from_dict_present_only_in_user_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert (
+        conf["dict_in_user"]["key_u1"]
+        == USER_CONFIG["dict_in_user"]["key_u1"])
+
+
+def test_add_item_to_dict_present_in_both_configs(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["dict_in_both"]["new_key"] = "new_value"
+    assert conf._user_dict["dict_in_both"]["new_key"] == "new_value"
+    assert "new_key" not in conf._default_dict["dict_in_both"]
+
+
+def test_add_item_to_dict_present_only_in_user_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["dict_in_user"]["new_key"] = "new_value"
+    assert conf._user_dict["dict_in_user"]["new_key"] == "new_value"
+    assert "dict_in_user" not in conf._default_dict
+
+
+def test_add_item_to_dict_present_only_in_default_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["dict_in_default"]["new_key"] = "new_value"
+    assert conf._user_dict["dict_in_default"]["new_key"] == "new_value"
+    assert (
+        conf._default_dict["dict_in_default"]
+        == DEFAULT_CONFIG["dict_in_default"])
+
+
+def test_create_no_user_config_file_if_default_values_unchanged(tmpdir):
+    _generate_default_config(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["string_in_default"] = DEFAULT_CONFIG["string_in_default"]
+    conf.save()
+    assert not conf.user_config_path.exists()
+
+
+def test_empty_existing_user_config_file_if_default_values_unchanged(tmpdir):
+    _generate_default_config(tmpdir)
+    conf = confjson.Config(tmpdir)
+    with conf.user_config_path.open("w") as file:
+        file.write("")
+    conf["string_in_default"] = DEFAULT_CONFIG["string_in_default"]
+    conf.save()
+    with conf.user_config_path.open() as file:
+        json_from_user_config_file = json.load(file)
+    assert json_from_user_config_file == {}
+
+
+def test_remove_items_from_user_config_if_identical_to_default_config(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert conf["string_in_both"] == USER_CONFIG["string_in_both"]
+    conf["string_in_both"] = DEFAULT_CONFIG["string_in_both"]
+    conf.save()
+    with conf.user_config_path.open() as file:
+        json_from_user_config_file = json.load(file)
+    assert "string_in_both" not in json_from_user_config_file
