1da26f8a8e436f1e1a528f456907cec4a7c1c01b
Namida Aneskans <namida@skunkfrukt.se>
2018-10-15 19:50:31 +0200

    Create tests for nested dict functionality.

--- tests/test_confjson.py
+++ tests/test_confjson.py
@@ -245,3 +245,22 @@ def test_delete_user_config_file_on_save_if_identical_to_default(tmpdir):
     assert conf.user_config_path.exists()
     conf.save()
     assert not conf.user_config_path.exists()
+
+
+def test_save_only_changed_items_in_nested_dicts(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    conf["dict_in_default"]["key_added_to_user"] = "krafs"
+    conf.save()
+    conf.load()
+    assert "dict_in_default" in conf._user_dict
+    assert len(conf._user_dict["dict_in_default"]) == 1
+    assert conf._user_dict["dict_in_default"]["key_added_to_user"] == "krafs"
+
+
+def test_access_nested_dicts_missing_in_user_but_present_in_default(tmpdir):
+    _generate_both_config_files(tmpdir)
+    conf = confjson.Config(tmpdir)
+    assert (
+        conf["dict_in_both"]["key_in_default"] ==
+        DEFAULT_CONFIG["dict_in_both"]["key_in_default"])
