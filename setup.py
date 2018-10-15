43c3caabc4ddf1727915c3cb96c1599d6ca05c31
Namida Aneskans <namida@skunkfrukt.se>
2018-10-15 19:22:40 +0200

    Version bump.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="confjson",
-    version="1.0.0",
+    version="1.0.1",
     author="Namida Aneskans",
     author_email="namida@skunkfrukt.se",
     description="A bafflingly simple, JSON-backed user configuration manager.",
